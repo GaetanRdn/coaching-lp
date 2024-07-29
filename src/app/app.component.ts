9afd2fdfc140f13814551e6343d656a991f6c127
gaetanredin <gr.recowd@gmail.com>
2024-07-29 15:46:39 +0200

    chore: init styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 @Component({
   standalone: true,
   selector: 'app-root',
-  template: '<h1>Hello there 👋🏼</h1>',
+  template: `<h1>Hello there 👋🏼</h1>`,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
